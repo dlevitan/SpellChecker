629570be5641489a57721f478bb2dd9eba80c1ab
dlevitan <dlevitan6623@gmail.com>
2019-07-03 07:48:12 -0500

    Client: File & Serve Xpress
    Updated for Core 3.0

---
+++ SpellChecker/Program.cs
@@ -0,0 +1,74 @@
+﻿using SpellChecker.Contracts;
+using SpellChecker.Core;
+using System.Linq;
+
+namespace SpellChecker.Console
+{
+
+    /// <summary>
+    /// The following are the "requirements" for this project:
+    /// 
+    /// 1. Implement Main() below so that a user can input a sentence.  Each word in that
+    ///    sentence will be evaluated with the SpellChecker, which returns true for a word
+    ///    that is spelled correctly and false for a word that is spelled incorrectly.  Display
+    ///    out each *distinct* word that is misspelled.  That is, if a user uses the same misspelled
+    ///    word more than once, simply output that word one time.
+    ///    
+    ///    Example:
+    ///    Please enter a sentence: Salley sells seashellss by the seashore.  The shells Salley sells are surely by the sea.
+    ///    Misspelled words: Salley seashellss
+    ///    
+    /// 2. The concrete implementation of SpellChecker depends on two other implementations of <see cref="ISpellChecker"/>, <see cref="DictionaryDotComSpellChecker"/>
+    ///    and MnemonicSpellCheckerIBeforeE.  You will need to implement those classes.  See those classes for details.
+    ///    
+    /// 3. There are covering unit tests in the SpellChecker.Tests library that should be implemented as well.
+    /// 
+    /// For extra credit, consider the following modifications:
+    /// 
+    /// 1. Convert to async.
+    /// 2. Implement Dependency Injection (framework of your choice).
+    /// 3. Dynamic loading of checking instances.
+    /// 
+    /// </summary>
+    public static class Program
+    {
+
+        /// <summary>
+        /// This application is intended to allow a user enter some text (a sentence)
+        /// and it will display a distinct list of incorrectly spelled words
+        /// </summary>
+        /// <param name="args"></param>
+        public static void Main(string[] args)
+        {
+            System.Console.Write("Please enter a sentence: ");
+
+            var sentence = System.Console.ReadLine();
+            // use this spellChecker to evaluate the words
+            var spellChecker = new Core.SpellChecker(new ISpellChecker[]
+            {
+                new MnemonicSpellCheckerIBeforeE(),
+                new DictionaryDotComSpellChecker(),
+            });
+
+            // first break the sentence up into words, - Parse by blank space
+            string[] sentenceWords = sentence.Split(' ');
+            ;
+
+            // then iterate through the list of words using the spell checker - loop through string array checking each distinct word, output miss spelled words
+            System.Console.Write("Miss Spelled Words: ");
+
+            foreach (string word2Check in sentenceWords.Distinct())
+            {
+                bool spelledRight = spellChecker.Check(word2Check);
+                if (spelledRight == false)
+                {
+                    // capturing distinct words that are misspelled
+                    System.Console.Write(word2Check + " ");
+                }
+
+            }
+        }
+
+    }
+
+}
---
+++ SpellChecker/SpellChecker.cs
@@ -0,0 +1,49 @@
+﻿using SpellChecker.Contracts;
+
+namespace SpellChecker.Core
+{
+
+    /// <summary>
+    /// This is a top level spell checker that is used by clients, it internally manages
+    /// several spell checkers that it uses to evaluate whether a word is spelled correctly
+    /// or not.
+    /// </summary>
+    public class SpellChecker :
+        ISpellChecker
+    {
+        private static ISpellChecker[] _spellCheckers;
+        readonly ISpellChecker[] spellCheckers;
+
+        /// <summary>
+        /// Initializes a new instance.
+        /// </summary>
+        /// <param name="spellCheckers"></param>
+        public SpellChecker(ISpellChecker[] spellCheckers)
+        {
+            _spellCheckers = spellCheckers;
+        }
+
+        /// <summary>
+        /// Iterates through all the internal spell checkers and returns false if any one of them finds a word to be
+        /// misspelled
+        /// </summary>
+        /// <param name="word">Word to check</param>
+        /// <returns>True if all spell checkers agree that a word is spelled correctly, false otherwise</returns>
+        public bool Check(string word)
+        {
+            //iterate through spell checkers checking "word"
+            //int i = spellCheckers.Length;
+            foreach (var testMethod in _spellCheckers)
+            {
+                if (testMethod.Check(word) == false)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+    }
+
+}
